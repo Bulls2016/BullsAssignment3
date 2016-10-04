cad4fffc9e73b709c9fea48363a1f03ac49dbcd8
BullsCSU <Ayesh@Ayeshs-MacBook-Pro.local>
2016-10-04 14:27:07 +1100

    Dynamic Testing

---
+++ Assignment3/library/entities/LoanTest.java
@@ -0,0 +1,14 @@
+package library.entities;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoanTest {
+
+	@Test
+	public void test() {
+		LoanTest test=new LoanTest();
+		assertEquals(test, test);
+	}
+
+}
